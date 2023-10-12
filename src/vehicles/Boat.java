8c26242d9b63725c634f20c5f8c2820f8bb3a214
Douglas Vinicius Dierings <146199335+Douglas2022322@users.noreply.github.com>
2023-10-12 16:49:45 +0100

    Added new classes
    Car, boat, plane

---
+++ src/vehicles/Boat.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vehicles;
+
+import oocminihw2.Sailable;
+import oocminihw2.Vehicle;
+
+/**
+ *
+ * @author dougl
+ */
+public class Boat extends Vehicle implements Sailable{
+
+    @Override
+    public void hoistSail() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void lowerSail() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean isSailHoisted() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void landHo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
